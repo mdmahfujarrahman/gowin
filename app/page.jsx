aa42db19a6cad4c59aee5fec4cb16b2b43c11b20
mdmahfujarrahman <ahmedmahfuj28@gmail.com>
2023-10-19 10:42:03 +0600

    css file update

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Button type="primary">Button</Button>
+      <Button type="primary">Button</Button>
     </main>
   );
 }
