b0d43532288a53778da5fc44624c0e5d5dbf62ed
mdmahfujarrahman <ahmedmahfuj28@gmail.com>
2023-10-22 09:19:02 +0600

    check

--- middleware.js
+++ middleware.js
@@ -2,13 +2,12 @@ import { NextResponse } from 'next/server';
 import { envConfig } from './lib/config';
 
 export async function middleware(request) {
-  const url = `${envConfig.serverUrl}/api/ipconfig`;
-  console.log(url);
+  const url = envConfig.ipconfig;
   try {
     const ipInfo = await fetch(url);
     if (ipInfo.status === 200) {
       const ipData = await ipInfo.json();
-      if (ipData?.data === 'BD') {
+      if (ipData.data.response.country_code === 'BD') {
         const url = request.nextUrl.clone();
         url.pathname = '/blocked-page';
         return NextResponse.rewrite(url);
